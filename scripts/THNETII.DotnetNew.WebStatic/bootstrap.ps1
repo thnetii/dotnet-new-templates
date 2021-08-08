375bc300268d3de9a44a709384d985d3531064e0
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-08-08 13:33:31 +0200

    Modify Program.cs to contain Startup code

--- scripts/THNETII.DotnetNew.WebStatic/bootstrap.ps1
+++ scripts/THNETII.DotnetNew.WebStatic/bootstrap.ps1
@@ -18,3 +18,144 @@ $LaunchSettingsJsonFilePath = Join-Path $PropertiesDirectory "launchSettings.jso
 $TemplateConfigDirectory = Join-Path $TargetDirectory ".template.config"
 New-Item -ItemType Directory $TemplateConfigDirectory -Force
 Copy-Item (Join-Path $PSScriptRoot "template.json") -Destination (Join-Path $TemplateConfigDirectory "template.json")
+
+### Read Startup.cs
+[System.IO.FileInfo]$StatupCsFileInfo = Get-Item (Join-Path $TargetDirectory "Startup.cs")
+$TempCommentLines = New-Object "System.Collections.Generic.List[string]"
+$ConfigureServicesCommentLines = New-Object "System.Collections.Generic.List[string]"
+$ConfigureServicesCodeLines = New-Object "System.Collections.Generic.List[string]"
+$ConfigureCommentLines = New-Object "System.Collections.Generic.List[string]"
+$ConfigureCodeLines = New-Object "System.Collections.Generic.List[string]"
+try {
+    $StartupCsReader = $StatupCsFileInfo.OpenText()
+    [string]$StartupCsLine = $null
+    while ($null -ne ($StartupCsReader.ReadLine() | Tee-Object -Variable StartupCsLine)) {
+        $StartupCsLineTrimmed = $StartupCsLine.Trim()
+        if ($StartupCsLine.StartsWith("//")) {
+            [void]$TempCommentLines.Add($StartupCsLineTrimmed)
+        }
+        elseif ($StartupCsLineTrimmed.StartsWith("public void ConfigureServices")) {
+            [void]$ConfigureServicesCommentLines.AddRange($TempCommentLines)
+            [void]$TempCommentLines.Clear()
+            [ValidateNotNull()][string]$StartupCsOpenCurlyLine = ""
+            do {
+                [ValidateNotNull()][string]$StartupCsOpenCurlyLine = $StartupCsReader.ReadLine()
+            } until ("{" -eq $StartupCsOpenCurlyLine.Trim())
+            [string]$IndentCurlyString = $StartupCsOpenCurlyLine.Substring(0, $StartupCsOpenCurlyLine.IndexOf("{"))
+            [string]$IndentCodeString = $IndentCurlyString + "    "
+            $StartupCsCloseCurlyLine = $IndentCurlyString + "}"
+            [ValidateNotNull()][string]$StartupCsCodeLine = ""
+            while ($true) {
+                [ValidateNotNull()][string]$StartupCsCodeLine = $StartupCsReader.ReadLine()
+                if ($StartupCsCodeLine.TrimEnd() -eq $StartupCsCloseCurlyLine) {
+                    break
+                }
+                $StartupCsCodeIndent = $StartupCsCodeLine.Substring(0, $IndentCodeString.Length)
+                [string]$StartupCsCodeLineUnindented = $null
+                if ([string]::IsNullOrWhiteSpace($StartupCsCodeIndent)) {
+                    $StartupCsCodeLineUnindented = $StartupCsCodeLine.Substring($IndentCodeString.Length)
+                }
+                else {
+                    $StartupCsCodeLineUnindented = $StartupCsCodeLine.TrimStart()
+                }
+                [void]$ConfigureServicesCodeLines.Add($StartupCsCodeLineUnindented)
+            }
+        }
+        elseif ($StartupCsLineTrimmed.StartsWith("public void Configure")) {
+            [void]$ConfigureCommentLines.AddRange($TempCommentLines)
+            [void]$TempCommentLines.Clear()
+            [ValidateNotNull()][string]$StartupCsOpenCurlyLine = ""
+            do {
+                [ValidateNotNull()][string]$StartupCsOpenCurlyLine = $StartupCsReader.ReadLine()
+            } until ("{" -eq $StartupCsOpenCurlyLine.Trim())
+            [string]$IndentCurlyString = $StartupCsOpenCurlyLine.Substring(0, $StartupCsOpenCurlyLine.IndexOf("{"))
+            [string]$IndentCodeString = $IndentCurlyString + "    "
+            $StartupCsCloseCurlyLine = $IndentCurlyString + "}"
+            [ValidateNotNull()][string]$StartupCsCodeLine = ""
+            while ($true) {
+                [ValidateNotNull()][string]$StartupCsCodeLine = $StartupCsReader.ReadLine()
+                if ($StartupCsCodeLine.TrimEnd() -eq $StartupCsCloseCurlyLine) {
+                    break
+                }
+                $StartupCsCodeIndent = $StartupCsCodeLine.Substring(0, $IndentCodeString.Length)
+                [string]$StartupCsCodeLineUnindented = $null
+                if ([string]::IsNullOrWhiteSpace($StartupCsCodeIndent)) {
+                    $StartupCsCodeLineUnindented = $StartupCsCodeLine.Substring($IndentCodeString.Length)
+                }
+                else {
+                    $StartupCsCodeLineUnindented = $StartupCsCodeLine.TrimStart()
+                }
+                [void]$ConfigureCodeLines.Add($StartupCsCodeLineUnindented)
+            }
+        }
+        else {
+            [void]$TempCommentLines.Clear()
+        }
+    }
+}
+finally {
+    if ($StartupCsReader) {
+        $StartupCsReader.Close()
+    }
+    Remove-Variable StartupCsReader
+    Remove-Variable TempCommentLines
+}
+[void]$StatupCsFileInfo.Delete()
+
+### Modify Program.cs
+[System.IO.FileInfo]$ProgramCsFileInfo = Get-Item (Join-Path $TargetDirectory "Program.cs")
+$ProgramCsLines = New-Object "System.Collections.Generic.List[string]"
+try {
+    $ProgramCsReader = $ProgramCsFileInfo.OpenText()
+    $SkipUsingsComplete = $false
+    [string]$ProgramCsLine = $null
+    while ($null -ne ($ProgramCsReader.ReadLine() | Tee-Object -Variable ProgramCsLine)) {
+        if (-not $SkipUsingsComplete) {
+            if ($ProgramCsLine.StartsWith("using")) {
+                continue # Ignore line
+            }
+            else {
+                [void]$ProgramCsLines.AddRange([string[]](Get-Content (Join-Path $PSScriptRoot "Program.usings.txt")))
+                [void]$ProgramCsLines.Add($ProgramCsLine)
+                $SkipUsingsComplete = $true
+            }
+        }
+        elseif ($ProgramCsLine.TrimStart().StartsWith("webBuilder.UseStartup<Startup>();")) {
+            $IndentCurlyString = $ProgramCsLine.Substring(0, $ProgramCsLine.IndexOf("webBuilder"))
+            $IndentCodeString = $IndentCurlyString + "    "
+            [void]$ProgramCsLines.AddRange([string[]]@(
+                "${IndentCurlyString}webBuilder.ConfigureServices(services =>",
+                "${IndentCurlyString}{"
+            ))
+            $ConfigureServicesCommentLines | ForEach-Object {
+                [void]$ProgramCsLines.Add("${IndentCodeString}$_")
+            }
+            $ConfigureServicesCodeLines | ForEach-Object {
+                [void]$ProgramCsLines.Add("${IndentCodeString}$_")
+            }
+            [void]$ProgramCsLines.Add("${IndentCurlyString}});")
+            [void]$ProgramCsLines.AddRange([string[]]@(
+                "${IndentCurlyString}webBuilder.Configure((context, app) =>",
+                "${IndentCurlyString}{"
+            ))
+            $ConfigureCommentLines | ForEach-Object {
+                [void]$ProgramCsLines.Add("${IndentCodeString}$_")
+            }
+            [void]$ProgramCsLines.Add("${IndentCodeString}var env = context.HostingEnvironment;")
+            $ConfigureCodeLines | ForEach-Object {
+                [void]$ProgramCsLines.Add("${IndentCodeString}$_")
+            }
+            [void]$ProgramCsLines.Add("${IndentCurlyString}});")
+        }
+        else {
+            [void]$ProgramCsLines.Add($ProgramCsLine)
+        }
+    }
+}
+finally {
+    if ($ProgramCsReader) {
+        $ProgramCsReader.Close()
+    }
+    Remove-Variable ProgramCsReader
+}
+Set-Content -LiteralPath $ProgramCsFileInfo.FullName $ProgramCsLines.ToArray() -Encoding utf8NoBOM -Force
